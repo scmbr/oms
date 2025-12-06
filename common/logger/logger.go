a0e038d7e8e30879c6af33377129bdbfc6ec9d64
scmbr <romvasily@mail.ru>
2025-12-06 21:05:46 +0900

    chore: update logger

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -1,11 +1,30 @@
 package logger
 
-import "github.com/rs/zerolog/log"
+import (
+	"os"
+	"time"
 
-func Info(msg string) {
-	log.Info().Msg(msg)
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func Init(serviceName string) {
+	zerolog.TimeFieldFormat = time.RFC3339
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).
+		With().Str("service", serviceName).Logger()
+}
+func Info(msg string, fields map[string]interface{}) {
+	event := log.Info()
+	for k, v := range fields {
+		event = event.Interface(k, v)
+	}
+	event.Msg(msg)
 }
 
-func Error(msg string, err error) {
-	log.Error().Err(err).Msg(msg)
+func Error(msg string, err error, fields map[string]interface{}) {
+	event := log.Error().Err(err)
+	for k, v := range fields {
+		event = event.Interface(k, v)
+	}
+	event.Msg(msg)
 }
