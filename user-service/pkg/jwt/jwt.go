36475663a9436639c7a9aee61984c431d7a0977b
scmbr <romvasily@mail.ru>
2025-12-08 22:48:11 +0900

    feat: add jwt manager

---
+++ user-service/pkg/jwt/jwt.go
@@ -0,0 +1,35 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type JWTManager struct {
+	secretKey string
+	ttl       time.Duration
+}
+
+func NewJWTManager(secretKey string, ttl time.Duration) *JWTManager {
+	return &JWTManager{secretKey: secretKey, ttl: ttl}
+}
+
+func (j *JWTManager) Generate(userID string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(j.ttl).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(j.secretKey))
+}
+
+func (j *JWTManager) Verify(tokenStr string) (*jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+		return []byte(j.secretKey), nil
+	})
+	if err != nil || !token.Valid {
+		return nil, err
+	}
+	return token.Claims.(*jwt.MapClaims), nil
+}
