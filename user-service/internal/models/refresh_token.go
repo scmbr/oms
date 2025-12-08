cbcbb3ea1c30a25c9f596a92365a908167afb62b
scmbr <romvasily@mail.ru>
2025-12-08 19:17:34 +0900

    feat: добавлены табличные наименования для Id

--- user-service/internal/models/refresh_token.go
+++ user-service/internal/models/refresh_token.go
@@ -7,8 +7,8 @@ import (
 )
 
 type RefreshToken struct {
-	ID        uuid.UUID `gorm:"type:uuid;primaryKey"`
-	UserID    uuid.UUID `gorm:"type:uuid;not null;index"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey;column:token_id"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null;index;column:user_id"`
 	Token     string    `gorm:"unique;not null"`
 	ExpiresAt time.Time `gorm:"not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
