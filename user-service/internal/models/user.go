bf2b377ffa31f8ce1f6a79eaf0240ede747de502
scmbr <romvasily@mail.ru>
2025-12-08 19:18:25 +0900

    feat: добавлены табличные наименования для id юзеров

--- user-service/internal/models/user.go
+++ user-service/internal/models/user.go
@@ -7,7 +7,7 @@ import (
 )
 
 type User struct {
-	ID           uuid.UUID `gorm:"type:uuid;primaryKey"`
+	ID           uuid.UUID `gorm:"type:uuid;primaryKey;column:user_id"`
 	Email        string    `gorm:"unique;not null"`
 	PasswordHash string    `gorm:"not null"`
 	Role         string    `gorm:"not null;default:user"`
