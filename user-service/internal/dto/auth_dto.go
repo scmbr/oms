93117f85995a0d8c22d34fa78b5d9497ffaf6ea2
scmbr <romvasily@mail.ru>
2025-12-08 22:36:45 +0900

    feat: add validation

--- user-service/internal/dto/auth_dto.go
+++ user-service/internal/dto/auth_dto.go
@@ -2,7 +2,7 @@ package dto
 
 type RegisterRequest struct {
 	Email    string `validate:"required,email"`
-	Password string `validate:"required,min=6"`
+	Password string `validate:"required,min=8"`
 }
 
 type LoginRequest struct {
--- user-service/internal/transport/grpc/user_handler.go
+++ user-service/internal/transport/grpc/user_handler.go
@@ -2,9 +2,14 @@ package grpc
 
 import (
 	"context"
+	"time"
 
+	"github.com/scmbr/oms/user-service/internal/dto"
 	pb "github.com/scmbr/oms/user-service/internal/pb"
 	"github.com/scmbr/oms/user-service/internal/service"
+	"github.com/scmbr/oms/user-service/internal/validation"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type UserHandler struct {
@@ -16,7 +21,16 @@ func NewUserHandler(userService *service.UserService) *UserHandler {
 	return &UserHandler{userService: userService}
 }
 func (h *UserHandler) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.AuthResponse, error) {
-	userDTO, err := h.userService.Register(ctx, req.Email, req.Password)
+	input := dto.RegisterRequest{
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	if err := validation.Validate.Struct(&input); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid input: %v", err)
+	}
+
+	userDTO, err := h.userService.Register(ctx, input.Email, input.Password)
 	if err != nil {
 		return nil, err
 	}
@@ -26,12 +40,20 @@ func (h *UserHandler) Register(ctx context.Context, req *pb.RegisterRequest) (*p
 		Email:            userDTO.Email,
 		Role:             userDTO.Role,
 		RefreshToken:     userDTO.RefreshToken,
-		RefreshExpiresAt: userDTO.RefreshExpiresAt.Format("2006-01-02T15:04:05Z07:00"),
+		RefreshExpiresAt: userDTO.RefreshExpiresAt.Format(time.RFC3339),
 	}, nil
 }
 
 func (h *UserHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.AuthResponse, error) {
-	userDTO, err := h.userService.Login(ctx, req.Email, req.Password)
+	input := dto.LoginRequest{
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	if err := validation.Validate.Struct(&input); err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid input: %v", err)
+	}
+	userDTO, err := h.userService.Login(ctx, input.Email, input.Password)
 	if err != nil {
 		return nil, err
 	}
