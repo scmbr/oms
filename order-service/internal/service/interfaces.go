62e1793e0c16c191b159c07abbc10af723724365
scmbr <romvasily@mail.ru>
2025-12-17 11:47:31 +0900

    fix: change service methods signature

--- order-service/internal/service/interfaces.go
+++ order-service/internal/service/interfaces.go
@@ -3,14 +3,13 @@ package service
 import (
 	"context"
 
-	"github.com/scmbr/oms/common/tx"
 	"github.com/scmbr/oms/order-service/internal/dto"
 	"github.com/scmbr/oms/order-service/internal/models"
 	"github.com/scmbr/oms/order-service/internal/repository"
 )
 
 type Order interface {
-	CreateOrder(ctx context.Context, userID string, items []models.OrderItem) (*dto.OrderDTO, error)
+	CreateOrder(ctx context.Context, userID string, items []dto.OrderItemDTO) (*dto.OrderDTO, error)
 	GetOrder(ctx context.Context, orderID string) (*dto.OrderDTO, error)
 	ListOrders(ctx context.Context, userID string) ([]dto.OrderDTO, error)
 	UpdateStatus(ctx context.Context, orderID string, newStatus models.OrderStatus, eventID string) error
@@ -19,16 +18,16 @@ type Order interface {
 
 type Outbox interface {
 	GetPending(ctx context.Context) ([]models.OutboxEvent, error)
-	MarkAsProcessed(ctx context.Context, txManager tx.TxManager, eventID string) error
+	MarkAsProcessed(ctx context.Context, eventID string) error
 }
 type Services struct {
 	Order  Order
 	Outbox Outbox
 }
 
-func NewServices(repo *repository.Repositories, txManager tx.TxManager) *Services {
+func NewServices(repo *repository.Repositories) *Services {
 	return &Services{
-		Order:  NewOrderService(repo, txManager),
+		Order:  NewOrderService(repo),
 		Outbox: NewOutboxService(repo),
 	}
 }
