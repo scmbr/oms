0707cf8579aaab4df47cb7856997f101205e8caa
scmbr <romvasily@mail.ru>
2025-12-18 21:21:00 +0900

    feat(inventory): add repository interfaces

---
+++ inventory-service/repository/interfaces.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/scmbr/oms/inventory-service/internal/models"
+	"gorm.io/gorm"
+)
+
+type Product interface {
+	Create(ctx context.Context, product *models.Product) (*models.Product, error)
+	GetById(ctx context.Context, productID string) (*models.Product, error)
+	GetAll(ctx context.Context) ([]models.Product, error)
+	Delete(ctx context.Context, productID string) (*models.Product, error)
+}
+type Stock interface {
+	Create(ctx context.Context, stock *models.Stock) (*models.Stock, error)
+	GetById(ctx context.Context, stockID string) (*models.Stock, error)
+	GetAll(ctx context.Context) ([]models.Stock, error)
+	Delete(ctx context.Context, stockID string) (*models.Stock, error)
+	UpdateQuantity(ctx context.Context, productID string, delta int) (*models.Stock, error)
+}
+type Reservation interface {
+	Create(ctx context.Context, reservation *models.Reservation) (*models.Reservation, error)
+	GetById(ctx context.Context, reservationID string) (*models.Reservation, error)
+	GetAll(ctx context.Context) ([]models.Reservation, error)
+	Delete(ctx context.Context, reservationID string) (*models.Reservation, error)
+	UpdateStatus(ctx context.Context, reservationID string, newStatus models.ReservationStatus) (*models.Reservation, error)
+}
+type Repository struct {
+	product     Product
+	stock       Stock
+	reservation Reservation
+}
+
+func NewRepository(db *gorm.DB) *Repository {
+	return &Repository{
+		product:     NewProductRepository(db),
+		stock:       NewStockRepository(db),
+		reservation: NewReservationRepository(db),
+	}
+}
