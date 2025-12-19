ff2c74c5d52a9cd5467eb0910d522479a7c4ede4
scmbr <romvasily@mail.ru>
2025-12-19 16:25:25 +0900

    fix(inventory): fix delete interfaces output

--- inventory-service/internal/repository/interfaces.go
+++ inventory-service/internal/repository/interfaces.go
@@ -11,20 +11,20 @@ type Product interface {
 	Create(ctx context.Context, product *models.Product) (*models.Product, error)
 	GetById(ctx context.Context, productID string) (*models.Product, error)
 	GetAll(ctx context.Context) ([]models.Product, error)
-	Delete(ctx context.Context, productID string) (*models.Product, error)
+	Delete(ctx context.Context, productID string) error
 }
 type Stock interface {
 	Create(ctx context.Context, stock *models.Stock) (*models.Stock, error)
 	GetById(ctx context.Context, productID string) (*models.Stock, error)
 	GetAll(ctx context.Context) ([]models.Stock, error)
-	Delete(ctx context.Context, stockID string) (*models.Stock, error)
+	Delete(ctx context.Context, stockID string) error
 	UpdateQuantity(ctx context.Context, productID string, delta int) (*models.Stock, error)
 }
 type Reservation interface {
 	Create(ctx context.Context, reservation *models.Reservation, externalID string) (*models.Reservation, error)
 	GetById(ctx context.Context, reservationID string) (*models.Reservation, error)
 	GetAll(ctx context.Context) ([]models.Reservation, error)
-	Delete(ctx context.Context, reservationID string) (*models.Reservation, error)
+	Delete(ctx context.Context, reservationID string) error
 	UpdateStatus(ctx context.Context, reservationID string, newStatus models.ReservationStatus) (*models.Reservation, error)
 }
 type Repository struct {
