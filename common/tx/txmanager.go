892547b7eb4bdb8aae2ca83cc2a60ea6af3b4c8c
scmbr <romvasily@mail.ru>
2025-12-25 18:23:29 +0900

    feat(common): add TxKey struct

--- common/tx/txmanager.go
+++ common/tx/txmanager.go
@@ -7,17 +7,24 @@ import (
 )
 
 type TxManager interface {
-	WithTx(ctx context.Context, fn func(tx *gorm.DB) error) error
+	WithTx(ctx context.Context, fn func(ctxTx context.Context) error) error
 }
 
 type txManager struct {
 	db *gorm.DB
 }
+type txKey struct{}
 
+func TxKey() any {
+	return txKey{}
+}
 func NewTxManager(db *gorm.DB) *txManager {
 	return &txManager{db: db}
 }
 
-func (m *txManager) WithTx(ctx context.Context, fn func(tx *gorm.DB) error) error {
-	return m.db.WithContext(ctx).Transaction(fn)
+func (m *txManager) WithTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	return m.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		ctxTx := context.WithValue(ctx, txKey{}, tx)
+		return fn(ctxTx)
+	})
 }
